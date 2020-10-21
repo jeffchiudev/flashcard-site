947cf8e062263ff1193bf2d286023a1600b97af3
Jeff Chiu <jwchiu82@gmail.com>
2020-10-20 20:38:03 -0700

    added individual card functionality with jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-    
-})
+  $(".clickable1").click(function() {
+    $(".card-text1").toggle();
+  });
+  $(".clickable2").click(function() {
+    $(".card-text2").toggle();
+  });
+  $(".clickable3").click(function() {
+    $(".card-text3").toggle();
+  });
+  $(".clickable4").click(function() {
+    $(".card-text4").toggle();
+  });
+});
